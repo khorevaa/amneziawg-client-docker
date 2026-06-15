4eb60eeeb31f83b0a8d3a4dcb6bf4113f73f9307
Aleksey Khorev <665525+khorevaa@users.noreply.github.com>
2026-06-16 02:39:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Переключаем iptables в режим legacy, чтобы избежать ошибки nf_tables на Synology
+if command -v update-alternatives &> /dev/null; then
+  update-alternatives --set iptables /usr/sbin/iptables-legacy || true
+  update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy || true
+elif [ -f /sbin/iptables-legacy ]; then
+  ln -sf /sbin/iptables-legacy /sbin/iptables
+  ln -sf /sbin/ip6tables-legacy /sbin/ip6tables
+fi
+
 # clear existing configurations
 find /etc/amnezia/amneziawg -mindepth 1 -delete
 
@@ -14,10 +23,22 @@ do
     echo awg interface "${name}" will be created from config file "${basename}"
     cp ${s} /etc/amnezia/amneziawg/${name}.conf
     chmod 600 /etc/amnezia/amneziawg/${name}.conf
+    
+    # Запуск интерфейса
     awg-quick up ${name}
+    
+    # Базовые правила форвардинга для контейнера
     iptables -A FORWARD -i ${name} -j ACCEPT
     iptables -A FORWARD -o ${name} -j ACCEPT
     iptables -A FORWARD -i ${name} -o ${name} -j ACCEPT
+    
+    # --- СВЯЗКА СО ВСТРОЕННЫМ OpenVPN SYNOLOGY ---
+    # Разрешаем трафик из интерфейса встроенного OpenVPN (tun0) в AmneziaWG
+    iptables -A FORWARD -i tun0 -o ${name} -j ACCEPT
+    iptables -A FORWARD -i ${name} -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+    
+    # Включаем NAT (маскарадинг), чтобы пакеты из встроенного OpenVPN (подсеть 10.8.0.0/24) уходили через AmneziaWG
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ${name} -j MASQUERADE
   fi
 done
 
@@ -25,5 +46,3 @@ if [[ $COUNTER -lt 1 ]]
 then
   echo "There are no config files in the /config folder"
 fi
-
-/bin/sh
